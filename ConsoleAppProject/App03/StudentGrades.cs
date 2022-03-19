6755fbf984e5ab5b311418fe01f9057f7e905f15
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-03-19 18:04:55 +0000

    App03 More Code Stuff

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using ConsoleAppProject.Helpers;
+using System.Linq;
 
 namespace ConsoleAppProject.App03
 {
@@ -20,15 +21,14 @@ namespace ConsoleAppProject.App03
         public const int MAX_PASS = 100;
 
         public string[] Students { get; set; }
-
         public int[] Marks { get; set; }
-
         public int[] GradeProfile { get; set; }
-
         public double Mean { get; set; }
-
-        public int Minimum { get; set; }
-        public int Maximum { get; set; }
+        public int Minimum { get; set; } = 0;
+        public int Maximum { get; set; } = 100;
+        public int MinMark { get; set; }
+        public int MaxMark { get; set; }
+        public int Percentage { get; set; } = 100;
 
 
 
@@ -42,6 +42,7 @@ namespace ConsoleAppProject.App03
                 Students = new string[] { "Jessica", "Alastair", "Grace", "Emma", "Oliver",
                     "Leo", "Charlie", "Matthew", "Delilah", "Lily", };
                 Marks = new int[Students.Length];
+                GradeProfile = new int[5];
 
                 ConsoleHelper.OutputHeading("App03 Student Marks", "Jessica Leach");
                 CalculateGrades();
@@ -52,8 +53,9 @@ namespace ConsoleAppProject.App03
         public void CalculateGrades()
         {
             InputMarks();
-            ConvertGrades();
+            //ConvertGrades();
             OutputMarks();
+            OutputStatisitcs();
         }
 
         private void InputMarks()
@@ -61,30 +63,35 @@ namespace ConsoleAppProject.App03
             Console.WriteLine("Please enter a mark for each student >\n");
             int i = 0;
 
-            foreach (var student in Students)
+            foreach (string name in Students)
             {
 
-                int mark = (int)ConsoleHelper.InputNumber( $"Enter a mark for {student}", 0, 100);
+                int mark = (int)ConsoleHelper.InputNumber( $"Enter a mark for student {name} > ", Minimum, Maximum);
                 Marks[i] = mark;
                 i++;
             }
         }
 
-        private void ConvertGrades()
+       // private void ConvertGrades()
+       
+       // {
 
-        {
-
-        }
+        //}
 
         private void OutputMarks()
         {
-            for(int i = 0; i < Marks.Length; i++)
+
+            ConsoleHelper.OutputTitle("List of Student Marks and Grades:");
+
+            for (int i = 0; i < Marks.Length; i++)
             {
-                int mark = Marks[i];
-                Grades grade = ConvertToGrades(mark);
-                Console.WriteLine($"{Students[i]} mark = {Marks} grade = {grade}");
+               
+                Grades outputGrade = CalculateGradeProfile(Marks[i]);
+                Console.WriteLine($"{Students[i]} mark = {Marks[i]} grade = {outputGrade}");
             }
         }
+
+
         public Grades ConvertToGrades(int mark)
         {
             if (mark >= 0 && mark <= MIN_D - 1)
@@ -104,5 +111,34 @@ namespace ConsoleAppProject.App03
 
             else return Grades.X;
         }
+
+     
+        public Grades CalculateGradeProfile(int mark)
+        { 
+            Grades grade = ConvertToGrades(mark);
+            GradeProfile[(int)grade - 1] = GradeProfile[(int)grade - 1] + (Percentage / Marks.Length);
+            return grade;
+        }
+
+
+        public void OutputStatisitcs()
+        {
+            Mean = (double)Marks.Sum() / (double)Marks.Length;
+            MinMark = Marks.Min();
+            MaxMark = Marks.Max();
+
+            ConsoleHelper.OutputTitle("Statistics:");
+            Console.WriteLine($"Mean Mark: {Mean} \nMinimum Mark: {MinMark} \nMaximum Mark: {MaxMark}");
+            ConsoleHelper.OutputTitle("Overall Grades:");
+          
+
+            //Runs a for loop to get the grade descriptor and percentage of people with that grade
+            for (int i = 0; i < GradeProfile.Length; i++)
+            {
+                Console.WriteLine($"Grade {Enum.GetName(typeof(Grades), i + 1)}-- {GradeProfile[i]}%");
+            }
+
+        }
+    
     }
 }
